e0b1f009d91225892abd6c9fddf1cc90a136ddd7
Chester Burbidge <chester.burbidge@gmail.com>
2016-01-14 01:21:30 +0000

    Added emailing stuff

--- ShamefulOldGit/Actors/EmailingActor.cs
+++ ShamefulOldGit/Actors/EmailingActor.cs
@@ -1,11 +1,16 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Mail;
 using Akka.Actor;
 
 namespace ShamefulOldGit.Actors
 {
 	public class EmailingActor : ReceiveActor
 	{
+		private const string EmailSubject = "Old un merged branches";
+		private const string EmailToAddress = "chester.burbidge@abide-financial.com";
+		private const string EmailFromAddress = "chester.burbidge@abide-financial.com";
 		private readonly IActorRef _lastEmailedFileActor;
 
 		public EmailingActor(IActorRef lastEmailedFileActor)
@@ -13,24 +18,35 @@ namespace ShamefulOldGit.Actors
 			_lastEmailedFileActor = lastEmailedFileActor;
 			Receive<PrinterActor.EmailContentToBeSent>(message =>
 			{
-				var path = Path.GetFullPath("..\\..\\..\\Email.html");
-				File.WriteAllText(path, message.Content);
-				Console.WriteLine($"Email file saved to location {path}");
+				var mail = new MailMessage();
+				var client = new SmtpClient();
+				var username = File.ReadAllText(GetPath("Username.txt"));
+				var password = File.ReadAllText(GetPath("Password.txt"));
+				client.Credentials = new NetworkCredential(username, password);
+				client.Port = 25;
+				client.Host = File.ReadAllText(GetPath("Host.txt"));
+				mail.To.Add(new MailAddress(EmailToAddress));
+				mail.From = new MailAddress(EmailFromAddress);
+				mail.Subject = EmailSubject;
+				mail.Body = message.Content;
+				client.Send(mail);
 
 				_lastEmailedFileActor.Tell(new EmailedAtTime(DateTime.Now));
 			});
 		}
 
+		private string GetPath(string path)
+		{
+			return Path.GetFullPath($"..\\..\\..\\{path}");
+		}
 		public class EmailedAtTime
 		{
-			public DateTime Now { get; set; }
-
 			public EmailedAtTime(DateTime now)
 			{
 				Now = now;
 			}
+
+			public DateTime Now { get; set; }
 		}
 	}
-
-	
 }
